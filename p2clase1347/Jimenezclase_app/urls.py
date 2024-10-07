a7d3fd03871205b29fbf732b3a7906ddc1eafc1d
Isaac-Jimenez-1347 <164533988+Isaac-Jimenez-1347@users.noreply.github.com>
2024-10-07 13:09:47 -0600

    Add files via upload

--- p2clase1347/Jimenezclase_app/urls.py
+++ p2clase1347/Jimenezclase_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.hola, name='Jimenezclase_app'),
-    path('minovia',views.minovia, name='minovia'),
+    path('',views.hola, name='hola'),
+    path('mimama/',views.Mi_mama, name='mimama'),
+    
 ]
